8a0c9cb6b83a779202ad29d561414e97dad3723c
Timi-G <56408619+Timi-G@users.noreply.github.com>
2024-09-20 16:22:04 +0100

    Update About.jsx
    Edited Nelius About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
         return (
             <main style={!isToggled ? dark : {}} className="about" id="about">
                 <h3 className="aboutHead">About <span className="textOne">Nelius</span></h3>
-                <p>Hosting events often requires financing and promotion. Nelius aims to facilitate funding and promotion efficiently. Its decentralized system utilizes technology, making it feasible to implement this goal. </p>
+                <p>Hosting events often requires financing and promotion. Nelius is a digital solution aiming to facilitate funding and efficient promotion of events.</p>
             </main>
         )
     };
